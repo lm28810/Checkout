9fd3bc2b5ffcfcbd97ce009960b1af153af4fa3b
Jazmine <lm28810@gmail.com>
2023-01-23 06:16:35 -0500

    final commit

--- src/Card.jsx
+++ src/Card.jsx
@@ -1,17 +1,37 @@
 import React from 'react'
+import Hero from './images/Hero.svg'
+import music from './images/icon-music.svg'
 import './Card.css'
+
+
 function Card() {
   return (
       <div className='Card-container'>
           <div className='Card-container-content'>
               <div>
-                   <h1>image here</h1>
+                   <img className='image1' src={Hero} alt="nothing" />
+                  {/* <h1>Image Here</h1> */}
               </div>
               <div>
                   <h2>Order Summary </h2>
-                  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Earum, temporibus pariatur voluptatibus officia quasi consequuntur dolore vero eius itaque quibusdam ad beatae nihil soluta inventore suscipit assumenda numquam voluptatum laboriosam?</p>
+                  <p className='Card-container-text'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. laboriosam?</p>
+              </div>
+              <div className='Card-container-details'>
+             
+                  <div >
+                      <img className="Image2"src={music} alt="" srcset="" />
+                 </div>
+                  <div>
+                      <h3 className='Plan'>Annual Plan </h3>
+                      <p className='Price'>$59.75 per/Year</p>
+                      </div>
+                  <div>
+                  
+                      <a className='change' href=""><p>Change</p></a>
+                  </div>
               </div>
               <div>
+            
                   <button>
                        Proceed to Payment
                   </button>
---
+++ src/Heros.jsx
@@ -0,0 +1,15 @@
+import Hero from './images/Hero.svg'
+import React from 'react'
+
+
+function Heros(){
+  
+  
+  return (
+    <div>
+        <img src={Hero} alt="nothing" />
+    </div>
+  )
+}
+
+export default Heros
